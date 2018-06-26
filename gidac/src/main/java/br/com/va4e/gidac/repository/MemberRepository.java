e9be5c4b389222ac6172d11b8ab705b4f2a6eed6
Dullio Macedo <dullio@dulliomacedo.com.br>
2018-06-26 15:00:08 -0300

    wip2

--- gidac/src/main/java/br/com/va4e/gidac/repository/MemberRepository.java
+++ gidac/src/main/java/br/com/va4e/gidac/repository/MemberRepository.java
@@ -3,7 +3,6 @@ package br.com.va4e.gidac.repository;
 import java.awt.print.Pageable;
 import java.util.List;
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -25,36 +24,18 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	long count() ;
 
-	void delete(Long id);
+
 	
 	void delete(Member entity);
 	
-	void delete(Iterable<? extends Member> entities);
-	
-	void deleteAll();
 
-	<S extends Member> S findOne(Example<S> example) ;
-	
-	<S extends Member> Page<S> findAll(Example<S> example, Pageable pageable) ;
-	
-	<S extends Member> long count(Example<S> example) ;
-
-	<S extends Member> boolean exists(Example<S> example);
-
-	Page<Member> findAll(Pageable pageable);
 	
 	List<Member> findAll();
 
 	List<Member> findAll(Sort sort) ;
 
 	
-	void flush() ;
 
-	<S extends Member> S saveAndFlush(S entity);
-
-	<S extends Member> List<S> findAll(Example<S> example);
-	
-	<S extends Member> List<S> findAll(Example<S> example, Sort sort);
 
 
 
