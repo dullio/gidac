378c6fed12f0ed4b37183bfeaec17cc664d7131c
Dullio Macedo <dullio@dulliomacedo.com.br>
2018-06-26 20:00:54 -0300

    Member Repository and Rest controller

--- gidac/src/main/java/br/com/va4e/gidac/repository/MemberRepository.java
+++ gidac/src/main/java/br/com/va4e/gidac/repository/MemberRepository.java
@@ -1,9 +1,7 @@
 package br.com.va4e.gidac.repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -15,28 +13,22 @@ import br.com.va4e.gidac.entity.Member;
 @RepositoryRestResource(path = "members",itemResourceRel = "members")
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	<S extends Member> S save(S entity);
+	//******************CRUD Methods***********************
 	
+	//READ
 	
 	Member findOne(Long id);
-
-	boolean exists(Long id);
-
-	long count() ;
-
-
-	
-	void delete(Member entity);
-	
-
 	
 	List<Member> findAll();
 
 	List<Member> findAll(Sort sort) ;
-
 	
+	//UPDATE and CREATE
+	
+	<S extends Member> S saveAndFlush(Member entity);
+	
+	//DELETE
 
-
-
-
+	void delete(Long id) ;
+	
 }
